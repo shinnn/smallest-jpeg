9577e3539e574bc9c98a03acdbf694515c4ec62e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-29 14:38:39 +0900

    use `Buffer.from` instead of a deprecated Buffer constructor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var SafeBuffer = require('safe-buffer').Buffer;
+
 module.exports = function smallestJpeg() {
-	return new Buffer('/9j/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/yQALCAABAAEBAREA/8wABgAQEAX/2gAIAQEAAD8A0s8g/9k=', 'base64');
+	return SafeBuffer.from('/9j/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/yQALCAABAAEBAREA/8wABgAQEAX/2gAIAQEAAD8A0s8g/9k=', 'base64');
 };
