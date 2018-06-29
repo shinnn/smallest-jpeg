4e7bcaac9fadb40e15cc36861272490b6bae39a7
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-29 14:51:02 +0900

    drop support for the environments where `Buffer.from` doesn't exist

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var SafeBuffer = require('safe-buffer').Buffer;
-
 module.exports = function smallestJpeg() {
-	return SafeBuffer.from('/9j/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/yQALCAABAAEBAREA/8wABgAQEAX/2gAIAQEAAD8A0s8g/9k=', 'base64');
+	return Buffer.from('/9j/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD/yQALCAABAAEBAREA/8wABgAQEAX/2gAIAQEAAD8A0s8g/9k=', 'base64');
 };
