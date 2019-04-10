5ec9642f7854411fb9e8011c2143b5cefa711609
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-04-10 21:38:05 +0900

    use `events.once()` to simplify the test
    https://nodejs.org/api/events.html#events_events_once_emitter_name

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const {get} = require('https');
+const {once} = require('events');
 
 const smallestJpeg = require('.');
 const test = require('tape');
 
 test('smallestJpeg()', async t => {
-	t.plan(1);
+	const [response] = await once(get('https://raw.githubusercontent.com/mathiasbynens/small/master/jpeg.jpg'), 'response');
+	const expected = [];
+	let len = 0;
 
-	get('https://raw.githubusercontent.com/mathiasbynens/small/master/jpeg.jpg', async response => {
-		const expected = [];
-		let len = 0;
+	for await (const chunk of response) {
+		expected.push(chunk);
+		len += chunk.length;
+	}
 
-		for await (const chunk of response) {
-			expected.push(chunk);
-			len += chunk.length;
-		}
+	t.ok(
+		smallestJpeg.equals(Buffer.concat(expected, len)),
+		'should bes a Buffer of the theoretically smallest JPEG.'
+	);
 
-		t.ok(
-			smallestJpeg.equals(Buffer.concat(expected, len)),
-			'should create a Buffer of the theoretically smallest JPEG.'
-		);
-	});
+	t.end();
 });
